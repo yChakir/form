4075cf5aabeaef2e0db9b573267932ed17ae952f
ychakir <yassine.chakir@ilemgroup.com>
2020-06-06 10:08:43 +0100

    Add boilerplate project. Closes #1

---
+++ src/main/java/ma/tiwtiw/form/Form.java
@@ -0,0 +1,13 @@
+package ma.tiwtiw.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Form {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Form.class, args);
+  }
+
+}
